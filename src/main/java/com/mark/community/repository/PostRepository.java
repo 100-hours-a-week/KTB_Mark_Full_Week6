a570fd441b8651bdd94b46b40eef8aadc2661744
juseungjuseung <juseungjuseung@gmail.com>
2026-06-21 17:02:32 +0900

    refactor: 게시글 레포지 토리 추가 및 페이징을 위한 메소드 추가

--- src/main/java/com/mark/community/repository/PostRepository.java
+++ src/main/java/com/mark/community/repository/PostRepository.java
@@ -1,12 +1,22 @@
 package com.mark.community.repository;
 
+
 import com.mark.community.entity.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface PostRepository {
-    Optional<Post> findById (String postId);
-    Post save(Post post);
-    Optional<List<Post>> findAllOrderByPostTime(int size, String lastPostId);
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.deleted = false AND p.temp = false ORDER BY p.id DESC")
+    List<Post> findPosts(Pageable pageable);
+
+    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.id < :lastPostId AND p.deleted = false AND p.temp = false ORDER BY p.id DESC")
+    List<Post> findPosts(@Param("lastPostId") Long lastPostId, Pageable pageable);
+
 }
