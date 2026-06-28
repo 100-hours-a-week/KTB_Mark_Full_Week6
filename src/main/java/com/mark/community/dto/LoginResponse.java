1dbec2c46a2f5f23144b48503add835d67fc4ffa
juseungjuseung <juseungjuseung@gmail.com>
2026-06-28 19:00:34 +0900

    refactor: LoginResponse DTO에 userId추가

--- src/main/java/com/mark/community/dto/LoginResponse.java
+++ src/main/java/com/mark/community/dto/LoginResponse.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public class LoginResponse {
     private Long profileFileId;
+    private Long userId;
 
-    public LoginResponse(Long profileFileId){
+    public LoginResponse(Long profileFileId, Long userId){
         this.profileFileId = profileFileId;
+        this.userId = userId;
     }
 }
