b8e344d57fe0a6bbe1f7a358d4d04e36ee8648cb
juseungjuseung <juseungjuseung@gmail.com>
2026-08-07 14:08:29 +0900

    feat: 게시글 조회시 category 조건 추가

--- src/main/java/com/mark/community/repository/PostRepository.java
+++ src/main/java/com/mark/community/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.mark.community.repository;
 
 
 import com.mark.community.entity.Post;
+import com.mark.community.enums.PostCategory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,11 +16,11 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.deleted = false AND p.temp = false ORDER BY p.id DESC")
-    List<Post> findPosts(Pageable pageable);
+    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.deleted = false AND p.temp = false AND (:category IS NULL OR p.category = :category) ORDER BY p.id DESC")
+    List<Post> findPosts(@Param("category") PostCategory category, Pageable pageable);
 
-    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.id < :lastPostId AND p.deleted = false AND p.temp = false ORDER BY p.id DESC")
-    List<Post> findPosts(@Param("lastPostId") Long lastPostId, Pageable pageable);
+    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.id < :lastPostId AND p.deleted = false AND p.temp = false AND (:category IS NULL OR p.category = :category) ORDER BY p.id DESC")
+    List<Post> findPosts(@Param("lastPostId") Long lastPostId, @Param("category") PostCategory category, Pageable pageable);
 
     @Query("SELECT COUNT(p) FROM Post p WHERE p.user.id = :userId AND p.temp = false AND p.postTime > :since")
     long countRecentPostsByUser(@Param("userId") Long userId, @Param("since") Date since);
