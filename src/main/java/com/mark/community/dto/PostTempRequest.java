78e7b983b92d799ed806691b3e27f5e2ddfbc97d
juseungjuseung <juseungjuseung@gmail.com>
2026-08-07 19:38:41 +0900

    feat: 게시글 수정 requestDTO에 category 추가

--- src/main/java/com/mark/community/dto/PostTempRequest.java
+++ src/main/java/com/mark/community/dto/PostTempRequest.java
@@ -4,10 +4,12 @@ package com.mark.community.dto;
 public class PostTempRequest {
     private String title;
     private String body;
+    private String category;
 
-    public PostTempRequest(String title, String body) {
+    public PostTempRequest(String title, String body, String category) {
         this.title = title;
         this.body = body;
+        this.category = category;
     }
 
     public String getTitle() {
@@ -15,5 +17,8 @@ public class PostTempRequest {
     }
     public String getBody() {
         return body;
+    }
+    public String getCategory() {
+        return category;
     }
 }
